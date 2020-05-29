f604c35bfe652d5edd2d8c7a9af0710cfaf18348
Kazeem Omoloja <omolojakazeem@gmail.com>
2020-05-29 05:05:12 +0100

    Editted Staticfiles Dir

--- fva_project/settings/base.py
+++ fva_project/settings/base.py
@@ -96,7 +96,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATIC_DIR = os.path.join(BASE_DIR,'static')
 STATICFILES_DIRS = [
     STATIC_DIR,
