6ecba55476ed8f9f8f3ab8e02f26581a47ab6426
Kazeem Omoloja <omolojakazeem@gmail.com>
2020-05-29 05:25:34 +0100

    Static Edit

--- fva_project/settings/prod.py
+++ fva_project/settings/prod.py
@@ -1,10 +1,11 @@
-import django_heroku
+from .base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['vgg_fva.herokuapp.com',]
+ALLOWED_HOSTS = ['vgg_fva.herokuapp.com', ]
 
 MY_SERVER = ALLOWED_HOSTS[0]
 
-django_heroku.settings(locals())
+import django_heroku
 
+django_heroku.settings(locals())
